53db00fdb690628b7e1cc76719c4dde36531fdee
Trevor-Koenig <trevorkoenig4@gmail.com>
2025-12-06 09:34:03 -0700

    updated carousel and annoying anchor tags

--- GRID/wwwroot/js/Pages/Index.js
+++ GRID/wwwroot/js/Pages/Index.js
@@ -1,6 +1,24 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.add('index-page');
 
+    // set up listeners for sections
+    // Select all headings that have an ID
+    const sections = document.querySelectorAll("section[id]");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const id = entry.target.id;
+                history.replaceState(null, null, "#" + id);
+            }
+        });
+    }, {
+        rootMargin: "0px 0px -70% 0px",   // Adjust which point marks a section as 'active'
+        threshold: 0
+    });
+
+    sections.forEach(section => observer.observe(section));
+
 });
 
 document.addEventListener('click', function (event) {
